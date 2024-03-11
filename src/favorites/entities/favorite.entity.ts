14d33c7c0ba5f80c458f800abd7e205b028955a3
VictoriaAlekseeva <vicalekseeva88@gmail.com>
2024-03-11 13:02:42 +0100

    refactor: remove comments

--- src/favorites/entities/favorite.entity.ts
+++ src/favorites/entities/favorite.entity.ts
@@ -10,10 +10,4 @@ export class FavoritesEntity implements Favorite {
   artists: string[] = []; // favorite artists ids
   albums: string[] = []; // favorite albums ids
   tracks: string[] = []; // favorite tracks ids
-
-  // constructor(favoritesData: Partial<FavoritesEntity>) {
-  //   this.artists = [...this.artists, ...favoritesData.artists];
-  //   this.albums = [...this.albums, ...favoritesData.albums];
-  //   this.tracks = [...this.tracks, ...favoritesData.tracks];
-  // }
 }
