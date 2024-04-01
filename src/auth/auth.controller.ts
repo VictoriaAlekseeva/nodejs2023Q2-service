239b87a4c781a9ca34399264bc8174af57f32b02
VictoriaAlekseeva <vicalekseeva88@gmail.com>
2024-04-01 21:48:06 +0200

    refactor: remove commented code

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -40,20 +40,3 @@ export class AuthController {
     return this.authService.refresh(updateAuthDto);
   }
 }
-
-// Signup (auth/signup route)
-// POST auth/signup - send login and password to create a new user
-// Server should answer with status code 201 and corresponding message if dto is valid
-// Server should answer with status code 400 and corresponding message if dto is invalid (no login or password, or they are not a strings)
-
-// Login (auth/login route)
-// POST auth/login - send login and password to get Access token and Refresh token (optionally)
-// Server should answer with status code 200 and tokens if dto is valid
-// Server should answer with status code 400 and corresponding message if dto is invalid (no login or password, or they are not a strings)
-// Server should answer with status code 403 and corresponding message if authentication failed (no user with such login, password doesn't match actual one, etc.)
-
-// Refresh (auth/refresh route)
-// POST auth/refresh - send refresh token in body as { refreshToken } to get new pair of Access token and Refresh token
-// Server should answer with status code 200 and tokens in body if dto is valid
-// Server should answer with status code 401 and corresponding message if dto is invalid (no refreshToken in body)
-// Server should answer with status code 403 and corresponding message if authentication failed (Refresh token is invalid or expired)
